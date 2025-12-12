627231332dcc6f63ef7ae86450a28643b96c4397
joao5435 <jv6858375@gmail.com>
2025-12-11 23:59:52 -0300

    Integração com IA funcionando

--- src/service/api.js
+++ src/service/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // URL do backend no Vercel
 const api = axios.create({
-  baseURL: "osg-backend.vercel.app",
+  baseURL: "https://osg-backend.vercel.app",
 });
 
 export default api;
